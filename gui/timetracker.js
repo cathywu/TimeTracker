107f7c54e06bf4b59c2150ffc5118c73c0d314f7
Pavel Panchekha <pavpanchekha@gmail.com>
2013-05-30 01:19:56 -0400

    Modified JS to use "counted" data throughout.  Much, much faster.

--- gui/timetracker.js
+++ gui/timetracker.js
@@ -6,51 +6,62 @@ RES = []
 
 function load_data(lines) {
     window.data = [];
+    last_title = void(0);
     for (var i in lines) {
         var parts = lines[i];
         var date = new Date(parts[0]);
         var title = parts[1];
-        window.data.push([date, title]);
+        if (title == last_title) {
+            window.data[window.data.length - 1][2] += 1
+        } else {
+            window.data.push([date, title, 1]);
+            last_title = title;
+        }
     }
 }
 
-var ms = 1000;
 function select_blocks(res) {
     var blocks = [];
     var total = 0;
     var last_time = 0;
 
-    function push(time, type) {
+    var s = 1000;
+
+    function push(time, type, number) {
+        start_time = time - 0; // Convert Date to *milliseconds* since epoch
+        end_time = start_time + number*s
+
         if (!blocks.length) {
-            last_time = time;
-            blocks.push([1, type, time, void(0)]);
-            total += 1;
+            last_time = end_time;
+            blocks.push([number, type, time, void(0)]);
+            total += number;
         } else {
-            if (time - last_time <= 30*ms) {
+            if (start_time - last_time <= 30*s) {
                 var last = blocks[blocks.length - 1];
                 if (last[1] == type) {
-                    last[0]++;
-                    total += 1;
+                    last[0] += number;
+                    total += number;
                 } else {
-                    last[3] = time + "";
-                    blocks.push([1, type, time, void(0)]);
-                    total += 1;
+                    last[3] = start_time;
+                    blocks.push([number, type, start_time, void(0)]);
+                    total += number;
                 }
             } else {
-                var skip = Math.round((time - last_time - 1*ms) / 1000);
-                blocks[blocks.length - 1][3] = last_time + 1*ms + "";
-                blocks.push([skip, "", last_time + 1*ms + "", time + ""]);
-                blocks.push([1, type, time, void(0)]);
-                total += skip + 1;
+                var skip = Math.round((start_time - last_time) / s);
+                blocks[blocks.length - 1][3] = last_time;
+                blocks.push([skip, "", last_time, time]);
+                blocks.push([number, type, start_time, void(0)]);
+                total += skip + number;
             }
 
-            last_time = time;
+            last_time = end_time;
         }
     }
     
     for (var i in window.data) {
         var date = window.data[i][0];
         var title = window.data[i][1];
+        var number = window.data[i][2];
 
         if (date < MINTIME || date >= MAXTIME) {
             continue;
@@ -59,14 +70,14 @@ function select_blocks(res) {
         var found = false;
         for (var j in res) {
             if (title.search(res[j]) > -1) {
-                push(date, j);
+                push(date, j, number);
                 found = true;
                 break;
             }
         }
 
         if (!found) {
-            push(date, "?");
+            push(date, "?", number);
         }
     }
 
@@ -101,32 +112,20 @@ function click_block() {
     var end = new Date($(this).data("end"));
 
     $("#blockinfo").empty();
-    var last_title = void(0);
-    var last_count = 0;
     for (var i in window.data) {
         var date = window.data[i][0];
         var title = window.data[i][1];
+        var number = window.data[i][2];
 
         if (date < start || date >= end) {
             continue;
         }
 
-        if (last_title == title) {
-            last_count ++;
-        } else {
-            if (last_title) {
-                var p = $("<p></p>").text(last_title + " ");
-                p.append($("<span>(" + last_count + "s)</span>").addClass("counter"));
-                $("#blockinfo").append(p);
-                last_count = 1;
-            }
-            last_title = title;
-        }
+        var duration = seconds_to_human_time(number);
+        var p = $("<p></p>").text(title + " ");
+        p.append($("<span>(" + duration + ")</span>").addClass("counter"));
+        $("#blockinfo").append(p);
     }
-
-    var p = $("<p></p>").text(title);
-    p.append($("<span>(" + last_count + "s)</span>").addClass("counter"));
-    $("#blockinfo").append(p);
 }
 
 function draw_blocks(res) {
