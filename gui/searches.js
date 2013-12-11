cc296790bff23b88b3d3c31aa7b5537055f29c97
Pavel Panchekha <pavpanchekha@gmail.com>
2013-12-10 23:22:06 -0800

    Added a new file for search mechanics.

---
+++ gui/searches.js
@@ -0,0 +1,74 @@
+// User queries and operations on them.
+
+// Selectors are functions that can select blocks
+
+Selectors = {
+    RE: function(re) {this.init(re)},
+};
+
+Selectors.RE.prototype.init = function(re) {
+    this.re = re;
+    this.text = re;
+};
+
+Selectors.RE.prototype.start = function(date, title, number) {
+    return title.search(this.re) > -1;
+};
+
+Selectors.RE.prototype.cont = Selectors.RE.prototype.start;
+
+// A Query is a selector and the selected blocks.
+
+function Query(text, data) {
+    this.text = text;
+    this.selector = this.parse(text);
+    this.blocks = [];
+    this.data = data;
+
+    this.total = 0;
+    this.hist = [ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+                , 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 ];
+}
+
+// Parses a string into selectors.
+// For now, just assumes that queries are regular expressions.
+
+Query.prototype.parse = function(text) {
+    return new Selectors.RE(text);
+}
+
+Query.prototype.block_to_events(block) {
+    return slice_data(block.start, block.end);
+}
+
+Query.prototype.add_block(block) {
+    // First, we record the existence of this block
+    this.blocks.push(block);
+
+    // Next, we add on the total time spent in this block
+    this.total += block.end - block.start;
+
+    // Next, we update the histogram
+    var seconds_in_hour = 60 * 60;
+    var prev_hour = Math.floor(block.start / seconds_in_hour) * seconds_in_hour;
+    var idx = (prev_hour / seconds_in_hour) % 24;
+
+    // There are a few cases here. The entire block may lie within
+    // a given hour or not; and the first and last hour that the block
+    // lies across are likely not to be full hours.
+
+    if (prev_hour + seconds_in_hour >= block.end) {
+        self.hist[idx] += block.length / seconds_in_hour;
+    } else {
+        var head = 1 - ((block.start - prev_hour) / seconds_in_hour);
+        self.hist[idx++] += head;
+        prev_hour += seconds_in_hour;
+        
+        while (prev_hour + seconds_in_hour < block.end) {
+            self.hist[idx++] += 1;
+        }
+
+        var tail = (block.end - prev_hour) / seconds_in_hour;
+        self.hist[idx] += tail;
+    }
+}
