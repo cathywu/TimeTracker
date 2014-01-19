37dd66ac7b867c82afbe6cdda01eb551dfa50d6b
Pavel Panchekha <me@pavpanchekha.com>
2014-01-19 02:06:28 -0800

    Refactored out some time code.

--- gui/init.js
+++ gui/init.js
@@ -5,34 +5,6 @@ DATA = null;
 START = 1e50;
 CDATA = null;
 
-function timerange_to_string(start, end) {
-    var title = start.format("[On] ll");
-    if (end.diff(start) < 2*60*1000) {
-        title += start.format(", [at] HH:mm [for] ") + end.from(start, true);
-    } else {
-        title += start.format(", [at] HH:mm–") + end.format("HH:mm");
-    }
-    return title;
-}
-
-function seconds_to_human_time(sec) {
-    function round_one_decimal_place(val) {
-        return Math.round(val * 10) / 10;
-    }
-
-    if (sec < 60) {
-        return sec + "s";
-    } else if (sec < 60 * 10) {
-        return round_one_decimal_place(sec / 60) + "m";
-    } else if (sec < 60 * 60) {
-        return Math.round(sec / 60) + "m";
-    } else if (sec < 60 * 60 * 10) {
-        return round_one_decimal_place(sec / (60 * 60)) + "hr";
-    } else {
-        return Math.round(sec / (60 * 60)) + "hr";
-    }
-}
-
 function on_new_search(evt) {
     evt.preventDefault();
     var input = $("#search").val();
---
+++ gui/time.js
@@ -0,0 +1,50 @@
+
+function seconds_to_human_time(sec) {
+    function round_one_decimal_place(val) {
+        return Math.round(val * 10) / 10;
+    }
+
+    if (sec < 60) {
+        return sec + "s";
+    } else if (sec < 60 * 10) {
+        return round_one_decimal_place(sec / 60) + "m";
+    } else if (sec < 60 * 60) {
+        return Math.round(sec / 60) + "m";
+    } else if (sec < 60 * 60 * 10) {
+        return round_one_decimal_place(sec / (60 * 60)) + "hr";
+    } else {
+        return Math.round(sec / (60 * 60)) + "hr";
+    }
+}
+
+function seconds_to_unit(sec, n) {
+    if (sec < 60*n) {
+        return "s";
+    } else if (sec < 60*60*n) {
+        return "m";
+    } else {
+        return "h";
+    }
+}
+
+function unit_to_seconds(u, n) {
+    n = n || 1;
+
+    if (u == "s") {
+        return n;
+    } else if (u == "m") {
+        return 60 * n;
+    } else if (u == "h") {
+        return 60 * 60 * n;
+    }
+}
+
+function timerange_to_string(start, end) {
+    var title = start.format("[On] ll");
+    if (end.diff(start) < 2*60*1000) {
+        title += start.format(", [at] HH:mm [for] ") + end.from(start, true);
+    } else {
+        title += start.format(", [at] HH:mm–") + end.format("HH:mm");
+    }
+    return title;
+}
