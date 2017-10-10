0a5b1786bd3cfa02c685eb6a7cd7a455ec917ef0
Pavel Panchekha <me@pavpanchekha.com>
2017-10-09 21:50:31 -0700

    Fixed bug in interpolation search and fixed search to binary

--- gui/searching.js
+++ gui/searching.js
@@ -1,18 +1,9 @@
 // Contains some functions to search arrays.
 //
-// Currently implements binary search and interpolation search.
-//
-// Should be used through array_search, which has some logic to select
-// the better algorithm.  It does look as though binary search really
-// is usually better.  Interpolation might be better for the inital
-// step where day sequences are split out, though.
+// Currently implements binary search (default) and interpolation search.
 
 function binary_search(data, value, lean) {
-    var time = 0;
-    
     function searcher(left_idx, right_idx) {
-        time++;
-        
         if (left_idx === right_idx) {
             return left_idx;
         } else if (left_idx + 1 === right_idx) {
@@ -46,19 +37,11 @@ function binary_search(data, value, lean) {
         }
     }
 
-    var out = searcher(0, data.length);
-    binary_search.times.count++;
-    binary_search.times.sum += time / Math.log(data.length);
-    return out;
+    return searcher(0, data.length);
 }
-binary_search.times = {sum: 0, count: 0}
 
 function interpolation_search(data, value, lean) {
-    var time = 0;
-    
     function searcher(left_idx, right_idx) {
-        time++;
-        
         if (left_idx === right_idx) {
             return left_idx;
         } else if (left_idx + 1 === right_idx) {
@@ -68,7 +51,7 @@ function interpolation_search(data, value, lean) {
             if (value < left_key) {
                 return left_idx;
             } else if (value > right_key) {
-                return right_key;
+                return right_idx;
             } else if (value === left_key) {
                 return left_idx;
             } else if (value === right_key) {
@@ -80,7 +63,7 @@ function interpolation_search(data, value, lean) {
             }
         } else {
             var left_key = data[left_idx];
-            var right_key = data[right_idx];
+            var right_key = right_idx < data.length ? data[right_idx] : data[right_idx - 1];
 
             var midpoint = Math.round((value - left_key) / (right_key - left_key)
                                       * (right_idx - left_idx) + left_idx);
@@ -99,42 +82,10 @@ function interpolation_search(data, value, lean) {
         }
     }
 
-    var out = searcher(0, data.length-1);
-    interpolation_search.times.count++;
-    interpolation_search.times.sum += time / Math.log(data.length);
-    return out;
+    return searcher(0, data.length);
 }
-interpolation_search.times = {sum: 0, count: 0}
 
-function array_search(data, value, lean) {
-    if (Math.random() < .1) {
-        var ans1 = binary_search(data, value, lean);
-        var ans2 = interpolation_search(data, value, lean);
-        if (ans1 != ans2) {
-            if (window.console && console.log) {
-                console.log("Interpolation and Binary Search disagree");
-                window.ERROR = {ans1: ans1, ans2: ans2,
-                                value: value, lean: lean,
-                                data: data};
-            }
-            interpolation_search.panic = true;
-        }
-
-        return ans1;
-    } else {
-        var binary_avg = binary_search.times.sum / binary_search.times.count;
-        var interp_avg = interpolation_search.times.sum / interpolation_search.times.count;
-        var fn;
-
-        if (interp_avg < binary_avg && !interpolation_search.times.panic) {
-            fn = interpolation_search;
-        } else {
-            fn = binary_search;
-        }
-
-        return fn(data, value, lean);
-    }
-}
+array_search = binary_search;
 
 function slice_data(data, start_t, end_t) {
     var left_idx  = array_search(data.times, start_t, "left");
