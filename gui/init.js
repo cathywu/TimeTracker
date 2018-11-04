b6cdf7712ccb833f32e518b379798d65e159eaf0
Pavel Panchekha <me@pavpanchekha.com>
2018-11-04 12:36:22 -0800

    Fix display but when deleting things

--- gui/init.js
+++ gui/init.js
@@ -242,6 +242,7 @@ $(function() {
         if (idx == -1) return;
         QUERIES.splice(idx, 1);
         q.selector.badge.remove();
+        $("#time .timeline, #time .empty-timeline").remove();
         draw_timelines(CDATA, QUERIES);
         $("#searchinfo").css("display", "none");
         $("#blockinfo").css("display", "none");
